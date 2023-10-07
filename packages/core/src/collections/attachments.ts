caa262a7bf8dcc7510d82aa6e6d869e390736dc8
Abdullah Atta <abdullahatta@streetwriters.co>
2023-10-07 13:56:24 +0500

    core: fix sync tests

--- packages/core/src/collections/attachments.ts
+++ packages/core/src/collections/attachments.ts
@@ -33,6 +33,7 @@ import { Output } from "../interfaces";
 import { Attachment } from "../types";
 import Database from "../api";
 import { SQLCollection } from "../database/sql-collection";
+import { isFalse } from "../database";
 
 export class Attachments implements ICollection {
   name = "attachments";
@@ -279,16 +280,9 @@ export class Attachments implements ICollection {
   }
 
   async attachment(hashOrId: string): Promise<Attachment | undefined> {
-    return await this.db
-      .sql()
-      .selectFrom("attachments")
-      .selectAll()
-      .where((eb) =>
-        eb.or([eb("id", "==", hashOrId), eb("hash", "==", hashOrId)])
-      )
-      .where("deleted", "is", null)
-      .$narrowType<Attachment>()
-      .executeTakeFirst();
+    return this.all.find((eb) =>
+      eb.or([eb("id", "==", hashOrId), eb("hash", "==", hashOrId)])
+    );
   }
 
   markAsUploaded(id: string) {
@@ -368,9 +362,7 @@ export class Attachments implements ICollection {
 
   get pending() {
     return this.collection.createFilter<Attachment>((qb) =>
-      qb.where((eb) =>
-        eb.or([eb("dateUploaded", "is", null), eb("dateUploaded", "<=", 0)])
-      )
+      qb.where(isFalse("dateUploaded"))
     );
   }
 
@@ -415,9 +407,11 @@ export class Attachments implements ICollection {
   //   );
   // }
 
-  // get all() {
-  //   return this.collection.items();
-  // }
+  get all() {
+    return this.collection.createFilter<Attachment>((qb) =>
+      qb.where(isFalse("deleted"))
+    );
+  }
 
   private async encryptKey(key: SerializedKey) {
     const encryptionKey = await this._getEncryptionKey();
